0329fa974104eb8f88e039849f1de61e55f04150
Ali <25alinadi>
2023-05-04 11:37:01 +0330

    create screen and patitions

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/chat_screen.dart';
+
+import 'common/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat GPT',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: scaffoldBackgroundColor,
+        appBarTheme: AppBarTheme(color: cardColor),
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
